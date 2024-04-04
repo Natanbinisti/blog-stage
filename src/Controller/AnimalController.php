057dd76e8dc5d92b7afc3bfa27a2990f3efd470d
natan <natanbinistilyon@gmail.com>
2024-04-04 20:32:38 +0200

    create

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Animal;
 use App\Form\AnimalType;
-use Doctrine\ORM\Mapping\Entity;
-use PhpParser\Node\Expr\New_;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,14 +20,19 @@ class AnimalController extends AbstractController
         ]);
     }
     #[Route('/animal/create', name: 'create')]
-    public function create(): Response
+    public function create(EntityManagerInterface $manager, Request $request): Response
     {
         $animal = New Animal();
         $formAnimal = $this->createForm(AnimalType::class,$animal);
+        $formAnimal->handleRequest($request);
 
+        if ($formAnimal->isSubmitted() && $formAnimal->isValid()){
+            $manager->persist($animal);
+            $manager->flush();
+        }
         return $this->render('animal/create.html.twig', [
             'controller_name' => 'AnimalController',
-            'formAnimal'=>$formAnimal
+            'formAnimal'=>$formAnimal->createView()
         ]);
     }
 }
